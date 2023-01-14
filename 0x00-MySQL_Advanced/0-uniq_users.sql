3337f0fb87d16ee9de85cf90f1930cec43079aca
bismarkboateng <bismarkb609@gmail.com>
2023-01-14 18:54:10 +0000

    task0

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,13 @@
+/*
+SQL script that creates a table users following these requirements:
+  id, integer, never null, auto increment and primary key
+  email, string (255 characters), never null and unique
+  name, string (255 characters) 
+*/
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email varchar(255) NOT NULL UNIQUE,
+    name varchar(255)
+);
