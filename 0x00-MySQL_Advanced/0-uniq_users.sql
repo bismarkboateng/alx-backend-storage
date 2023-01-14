374bb2175ad5a4c422e11e3167840b24edffc3f7
bismarkboateng <bismarkb609@gmail.com>
2023-01-14 19:08:35 +0000

    task0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,11 +1,8 @@
 --Creates users table  
 
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE 
-    users (
-        id INT NOT NULL AUTO_INCREMENT,
-        email VARCHAR(255) NOT NULL UNIQUE,
-        name VARCHAR(255),
-        PRIMARY KEY (id)
-    );
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
